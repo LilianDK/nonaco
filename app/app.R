e89f94e41208319f315b742fa4afad4967480b21
Lili <lilian.do.khac@rwth-aachen.de>
2024-04-28 15:59:42 +0200

    contact elements

--- app/app.R
+++ app/app.R
@@ -6,13 +6,12 @@ library(shinyBS)
 ui <- fluidPage(
   titlePanel(
     title =
-      tags$link(rel = "icon", type = "image/gif", href = "https://guidetoiceland.is/image/389003/x/0/the-beautiful-waterfalls-of-south-iceland-seljalandsfoss-skogafoss-amp-gljufrabui-1.jpg"),
-    "My App"
+      tags$link(rel = "icon", type = "image/png", href = "https://raw.githubusercontent.com/LilianDK/nonaco/main/images/logo.png"),
+    "cohano"
   ),
-  #setBackgroundImage(src = "https://images.unsplash.com/photo-1507738978512-35798112892c?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-  
+
   navbarPage(title = "cohano",
-         theme = "style/style.css",
+         theme = "",
          footer = includeHTML("footer.html"),
          fluid = TRUE,
          collapsible = TRUE,
@@ -32,8 +31,6 @@ ui <- fluidPage(
          # tabe panel 2
          tabPanel("Publikationen",
          ),
-         tags$head(tags$style('.navbar-nav {width: 95%;}
-                                    .navbar-nav :third-child{float:right}'))
   )
 )
 
