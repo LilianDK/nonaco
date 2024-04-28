50f54e02f3e29d7b03eb6b1462480c167a8460f7
Lili <lilian.do.khac@rwth-aachen.de>
2024-04-28 10:36:29 +0200

    init

---
+++ app/app.R
@@ -0,0 +1,42 @@
+library(shiny)
+library(shinyWidgets)
+library(shinyBS)
+
+# UI ----
+ui <- fluidPage(
+  #setBackgroundImage(src = "https://images.unsplash.com/photo-1507738978512-35798112892c?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+  
+  navbarPage(title = "nonaco",
+         theme = "style/style.css",
+         footer = includeHTML("footer.html"),
+         fluid = TRUE,
+         collapsible = TRUE,
+  
+         # ----------------------------------
+         # tabe panel 1
+         tabPanel("Home",
+                  includeHTML("home.html")
+         ),
+         
+         # ----------------------------------
+         # tabe panel 2
+         tabPanel("Portfolio",
+         ),
+         
+         # ----------------------------------
+         # tabe panel 2
+         tabPanel("Publikationen",
+         ),
+         
+  )
+)
+
+# SERVER ----
+server <- function(input, output) {
+}
+
+#shinylive::export(appdir = "app", destdir = "docs")
+shinyApp(ui = ui, server = server)
+
+
+
