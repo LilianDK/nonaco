99ea1b445e342172e6c9441e4487ed281de1a587
Lili <lilian.do.khac@rwth-aachen.de>
2024-05-01 21:48:24 +0200

    shinyexport

--- app/app.R
+++ app/app.R
@@ -1,9 +1,10 @@
 library(shiny)
 library(shinyWidgets)
 library(shinyBS)
-#install.packages("superml")
 library(superml)
 library(DT)
+
+source("data.R")
 
 # UI ----
 ui <- fluidPage(
@@ -49,8 +50,29 @@ ui <- fluidPage(
                       div(class="panel-body",  
                           tags$div( align = "center",
                                     div( align = "left", 
-                                         h5("Test"),
-                                         DT::DTOutput("ranking")
+                                         h4(strong(textOutput("title_1"))),
+                                         fluidRow(
+                                           column(3,
+                                                  "Language: ", textOutput("language_1", inline = TRUE)),
+                                           column(3,
+                                                  "Publication date:", textOutput("date_1", inline = TRUE)),
+                                           column(3,
+                                                  "Publication outlet:", textOutput("outlet_2", inline = TRUE)),
+                                           column(3,)
+                                         ),
+                                         fluidRow(
+                                           column(12,
+                                                  "Abstract:", textOutput("abstract_1"))
+                                         ),
+                                         fluidRow(
+                                           column(12,
+                                                  "Link:", textOutput("url_1", inline = TRUE))
+                                         ),
+                                         fluidRow(
+                                           column(12,
+                                                  "BM25 score:", textOutput("rank_1", inline = TRUE))
+                                         ),
+                                         #DT::DTOutput("ranking")
                                     )
                           ),
                           tags$p(h6("")),
@@ -60,7 +82,28 @@ ui <- fluidPage(
                       div(class="panel-body",  
                           tags$div( align = "center",
                                     div( align = "left", 
-                                         h5("Test")
+                                         h4(strong(textOutput("title_2"))),
+                                         fluidRow(
+                                           column(3,
+                                                  "Language: ", textOutput("language_2", inline = TRUE)),
+                                           column(3,
+                                                  "Publication date:", textOutput("date_2", inline = TRUE)),
+                                           column(3,
+                                                  "Publication outlet:", textOutput("outlet_2", inline = TRUE)),
+                                           column(3,)
+                                         ),
+                                         fluidRow(
+                                           column(12,
+                                                  "Abstract:", textOutput("abstract_2"))
+                                         ),
+                                         fluidRow(
+                                           column(12,
+                                                  "Link:", textOutput("url_2", inline = TRUE))
+                                         ),
+                                         fluidRow(
+                                           column(12,
+                                                  "BM25 score:", textOutput("rank_2", inline = TRUE))
+                                         )
                                     )
                           ),
                           tags$p(h6("")),
@@ -75,7 +118,6 @@ ui <- fluidPage(
 server <- function(input, output) {
 
   output$ranking <- DT::renderDT({  
-    source("data.R")
     
     sentence <- input$search
     test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
@@ -89,9 +131,245 @@ server <- function(input, output) {
     
     output <- merge(docs, vector, by = "title")
     output <- output[order(output$rank, decreasing = TRUE),]
-    #title <- output[1,1]
   })
 
+  output$title_1 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    title <- output[1,1]
+  })
+  
+  output$title_2 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    title <- output[2,1]
+  })
+  
+  output$outlet_1 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    outlet <- output[1,2]
+  })
+  
+  output$outlet_2 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    outlet <- output[2,2]
+  })
+  
+  output$language_1 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    language <- output[1,4]
+  })
+  
+  output$language_2 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    language <- output[2,4]
+  })
+  
+  output$date_1 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    date <- output[1,3]
+  })
+  
+  output$date_2 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    date <- output[2,3]
+  })
+  
+  output$url_1 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    url <- output[1,5]
+  })
+  
+  output$url_2 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    url <- output[2,5]
+  })
+  
+  output$rank_1 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    rank <- output[1,7]
+  })
+  
+  output$rank_2 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    rank <- output[2,7]
+  })
+  
+  output$abstract_1 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    rank <- output[1,6]
+  })
+  
+  output$abstract_2 <- renderText({  
+    
+    sentence <- input$search
+    test <- bm_25(document=sentence, corpus=docs[,1], top_n = 2)
+    
+    vector <- data.frame(title = character(0), rank = numeric(0))
+    for (i in 1:length(test)) {
+      row <- cbind(names(test)[[i]], unname(test)[[i]])
+      vector <- rbind(vector, row)
+    }
+    names(vector) <- c("title", "rank")
+    
+    output <- merge(docs, vector, by = "title")
+    output <- output[order(output$rank, decreasing = TRUE),]
+    rank <- output[2,6]
+  })
 }
 #shinylive::export(appdir = "app", destdir = "docs")
 shinyApp(ui = ui, server = server)
--- app/data.R
+++ app/data.R
@@ -1,9 +1,9 @@
 # Publications
 
-pub1 <- c("Chancen auf eine sichere digitale Welt mit KI", "DIGITALE WELT", "29.03.2024", "German", "https://digitaleweltmagazin.de/chancen-auf-eine-sichere-digitale-welt-mit-ki/")
-pub2 <- c("Übersicht zur neuen U.S. KI Executive Order", "Trend Report", "6.03.2024", "German", "https://trendreport.de/uebersicht-zur-neuen-executive-order/")
+pub1 <- c("Chancen auf eine sichere digitale Welt mit KI", "DIGITALE WELT", "29.03.2024", "German", "https://digitaleweltmagazin.de/chancen-auf-eine-sichere-digitale-welt-mit-ki/", "Die voranschreitende Durchdringung von Anwendungen mit Künstlicher Intelligenz (KI) in Produkten und Dienstleistungen tritt in die nächste Phase des digitalen Zeitalters ein. Damit werden KI-Anwendungen immer mehr zur Grundlage für wirtschaftliche Aktivitäten, sogar unabdingbar für den wirtschaftlichen Erfolg. Diese möchte der Gesetzgeber regulieren, um Risiken vorzubeugen. ")
+pub2 <- c("Übersicht zur neuen U.S. KI Executive Order", "Trend Report", "6.03.2024", "German", "https://trendreport.de/uebersicht-zur-neuen-executive-order/", "Für die Verarbeitung von Daten nutzen User und Unternehmen tagtäglich U.S.-Clouddienste. Oder amerikanische Rechendienstleistungen, um eigene Daten oder Daten aus den Datenzentren der Kunden an andere Datenzentren zu senden, dort zu verarbeiten, und anschließend wieder fertig verarbeitet entgegenzunehmen. Neben diesen Datendienstleistungen werden U.S.-Clouddienste genutzt, um Rechenleistungen für KI-Modelle zu beziehen. Am 30. Oktober 2023 erließ das Weiße Haus die „Executive Order on the Safe, Secure, and Trustworthy Development and Use of Artificial Intelligence“.  Dies ist ein einschlägiges Gesetz, welches unter anderem den U.S.-Clouddiensten weitere Transparenzpflichten auferlegt, die auch weltweit Unternehmen und Kunden betreffen. Nachfolgend wird eine Übersicht zu dieser Executive Order gegeben und ein kritischer Punkt hervorgehoben.")
 
 docs <- data.frame()
 docs <- rbind(docs, pub1, pub2)
-names(docs) <- c("title", "outlet", "date", "langauge", "url")
+names(docs) <- c("title", "outlet", "date", "language", "url", "abstract")
 
